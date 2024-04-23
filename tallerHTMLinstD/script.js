886ad14f9451e3134ff096921b3c61bad214a21e
Pzjz <andrespzjz123@gmail.com>
2024-04-23 17:25:23 -0500

    Html

---
+++ tallerHTMLinstD/script.js
@@ -0,0 +1,12 @@
+function irALink() {
+    window.location.href = "noticia1.html";
+}
+function irALink2() {
+    window.location.href = "noticia2.html";
+}
+function irALink3() {
+    window.location.href = "noticia3.html";
+}
+function irALink4() {
+    window.location.href = "noticia4.html";
+}
